e082860a48bc3bf62bcc3b174d7e29cff24a8102
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2022-03-31 10:10:37 -0500

    Removed old files

--- kernel/lib/string.cpp
+++
@@ -1,8 +0,0 @@
-#include "string.h"
-#include <arch/interface/properties.h>
-
-void * memcpy ( void * destination, const void * source, size_t num )
-{
-    if constexpr(properties.test(HAS_MEMCPY))
-        return archlib::memcpy(destination, source, num);
-}
--- kernel/lib/string.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LIB_STRING_H__
-#define __LIB_STRING_H__
-
-void * memcpy ( void * destination, const void * source, size_t num );
-
-#endif
