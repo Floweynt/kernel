c1954299ac48a7db05374ccd11f6cf9463e825ba
Cedric Xia <xiacedric909@gmail.com>
2023-11-28 18:53:47 -0600

    driver: Refactoring and style guide

--- kernel/src/arch/x86/drivers/keyboard/ps.cpp
+++ kernel/src/arch/x86/drivers/keyboard/ps.cpp
@@ -1,28 +1,49 @@
+#include <array>
 #include <asm/asm_cpp.h>
 #include <cstdint>
 #include <klog/klog.h>
-std::uint8_t decode_keyboard_scancode(std::uint8_t scancode, const std::uint8_t* translation_table, const std::uint8_t* translation_table_end,
+#include <span>
+static constexpr auto TRANSLATION_TABLE_SIZE = 128;
+std::uint8_t decode_keyboard_scancode(std::uint8_t scancode, const std::array<std::uint8_t, TRANSLATION_TABLE_SIZE>& translation_table,
                                       const std::uint8_t flag_pressed)
+// std::uint8_t decode_keyboard_scancode(std::uint8_t scancode, std::span<uint8_t, TRANSLATION_TABLE_SIZE> &translation_table, const std::uint8_t
+// flag_pressed)
 {
     std::uint8_t decoded_keystroke = 0;
     // bool is_pressed = !(scancode & flag_pressed); // false means released
     //  klog::log("SCANCODE:: %2X, %2X", scancode, scancode & (~flag_pressed));
     //  klog::log("SIZE:: %d", table_size);
     std::uint8_t key_scancode = scancode & (~flag_pressed); // Stripped of press flag
-    if ((scancode & (~flag_pressed)) < ((translation_table_end - translation_table) / sizeof(translation_table[0])))
-    {
-        // klog::log("HERE!!!!!");
-        decoded_keystroke = translation_table[scancode & (~flag_pressed)];
-    }
+    // if ((scancode & (~flag_pressed)) < ((translation_table_end - translation_table) / sizeof(translation_table[0])))
+    (key_scancode < (translation_table.size())) && (decoded_keystroke = translation_table[key_scancode]);
     // klog::log("%2X, %d", decoded_keystroke, is_pressed);
     return decoded_keystroke;
 }
-void poll_keystroke(const std::uint8_t* translation_table, const std::uint8_t* translation_table_end)
+
+// void poll_keystroke(const std::uint8_t* translation_table, const std::uint8_t* translation_table_end)
+void poll_keystroke()
 {
+
+    static constexpr std::array<std::uint8_t, TRANSLATION_TABLE_SIZE> us_qwerty_translation = {
+        0, 0x1B, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0x08, 0x09, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']',
+        0x0D, 0x11, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`', 0x0E, '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 0x0F,
+        '*', 0x13, ' ', 0x02,
+        // function keys (F1-F10) here
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        // Numlock and Scroll Lock:
+        0, 0, '7', '8', '9', '-', '4', '5', '6', '+', '1', '2', '3', '0', '.',
+        // three gaps
+        0, 0, 0,
+        // F11, F12
+        0, 0};
+    auto& translation_table = us_qwerty_translation;
+    // auto translation_table =us_qwerty_translation;
+    // auto translation_table_end = us_qwerty_translation + sizeof(us_qwerty_translation)/sizeof(us_qwerty_translation[0]);
+    static constexpr std::uint8_t FLAG_PRESSED = 0x80;
     auto prev_msg = 0;
     while (true)
     {
-        auto scancode = inb(ioports::KEYBOARD);
+        auto scancode = inb(ioports::PS2_KEYBOARD_DATA);
         if (scancode == prev_msg)
             continue;
         else
@@ -38,11 +59,9 @@ void poll_keystroke(const std::uint8_t* translation_table, const std::uint8_t* t
          * TODO eventually: distinguish Keypad / (0xE0, 0x35) and enter (0xE0, 0x1C) from normal, and RIGHT CTRL and ALT from LEFT
          */
         std::uint8_t decoded_keystroke;
-        static constexpr std::uint8_t FLAG_PRESSED = 0x80;
         bool is_pressed = !(scancode & FLAG_PRESSED);
 
-        decoded_keystroke = decode_keyboard_scancode(
-            scancode, translation_table, translation_table_end, FLAG_PRESSED);
+        decoded_keystroke = decode_keyboard_scancode(scancode, translation_table, FLAG_PRESSED);
         if (decoded_keystroke != 0)
         {
             // THIS IS A TEMPORARY SWITCH CASE FOR DEBUGGING, TODO: DELETE IT AND ACTUALLY HANDLE STUFF
